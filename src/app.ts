b0960ee57f3a84a297aee67bc8f846663f44ad20
hieunguyenvan <hieu.nguyenvan@powergateaustralia.com>
2024-01-09 16:42:47 +0700

    config socket

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,15 @@
-import * as express from "express";
-import { createServer } from 'http';
+require("dotenv").config();
+import "module-alias/register";
+import "reflect-metadata";
+
+import express from "express";
+import { createServer } from "http";
+import { SocketService } from "@libs/socket/socket"; 
 
 const app = express();
-const server = createServer(app);
-server.listen(3000, () => {
+
+const httpServer = createServer(app);
+const socket = SocketService.getInstanceSocket(httpServer);
+httpServer.listen(3000, () => {
   console.log(`Server is running in port ${3000}`);
 });
--- src/libs/socket/socket.ts
+++ src/libs/socket/socket.ts
@@ -1,21 +1,28 @@
 import { Server } from 'socket.io';
+import { Server as HttpServer} from 'http';
+import { ClientToServerEvents, InterServerEvents, ServerToClientEvents, SocketData } from './interfaces/socket.interface';
 
 const WEBSOCKET_CORS = {
   origin: "*",
   methods: ["GET", "POST"],
 };
 
-export class SocketService extends Server {
+export class SocketService extends Server<
+  ServerToClientEvents,
+  ClientToServerEvents,
+  InterServerEvents,
+  SocketData
+> {
   private static socketInstance: SocketService;
-  private constructor (httpServer) {
-    super(httpServer, {
-      cors: WEBSOCKET_CORS
-    })
+  private constructor(app: HttpServer) {
+    super(app, {
+      cors: WEBSOCKET_CORS,
+    });
   }
 
-  static getInstanceSocket(httpServer): SocketService {
+  static getInstanceSocket(app: HttpServer): SocketService {
     if (!this.socketInstance) {
-      this.socketInstance = new SocketService(httpServer);
+      this.socketInstance = new SocketService(app);
     }
     return this.socketInstance;
   }
